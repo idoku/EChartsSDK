4a546781653055c18618692711d58c280417d1e8
idoku <dragon.ice.995@hotmail.com>
2016-01-12 10:15:01 +0800

    init

---
+++ EChartsSDK/ECharts/Entities/series/Bar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECharts.Entities.series
+{
+    public class Bar : Rectangular<Bar>
+    {
+        public object barGap { get; set; }
+
+        public object barCategoryGap { get; set; }
+
+        public int? barMinHeight { get; set; }
+
+        public int? barWidth { get; set; }
+
+        public int? barMaxWidth { get; set; }
+
+        public Bar() {
+            this.type = ChartType.bar;
+        }
+
+        public Bar(string name) : this() {
+            this.name = name;
+        }
+    }
+}
